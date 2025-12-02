22bb993345bd2aefab63074ee20f93e0dc8279fc
Matthew-Sher <gomaruz75@gmail.com>
2025-12-02 12:28:31 +0300

    Added animated disappearance and appearance for background computer

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let aboutSectionTitle = document.querySelector("#about-section .secondary-title");
+let backgroundAbovePC = document.querySelector('.background-glow-after-scroll');
 
 window.addEventListener('scroll', () => {
-    if (document.documentElement.scrollTop > 60) {
+    let scrollScale = document.documentElement.scrollTop;
+
+    if (scrollScale > 60) {
         aboutSectionTitle.classList.add('animated-apearence');
         setTimeout(() => {
             aboutSectionTitle.style.setProperty('opacity', '1');
@@ -10,4 +13,12 @@ window.addEventListener('scroll', () => {
         aboutSectionTitle.style.setProperty('opacity', '0');
         aboutSectionTitle.classList.remove('animated-apearence');
     }
+
+    if (scrollScale > 100) {
+        opacityScale = (scrollScale - 100) / 600;
+        backgroundAbovePC.style.setProperty('opacity', opacityScale);
+    } else {
+        backgroundAbovePC.style.setProperty('opacity', '0');
+    }
 });
+
