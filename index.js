d900028ec74c298a064a2ffdd957a2b36bc1d8ae
Matthew-Sher <gomaruz75@gmail.com>
2026-02-27 12:30:38 +0300

    Added empty skill section, added pc blinking, changed --acent-primary-color

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-let aboutSectionTitle = document.querySelector("#about-section .secondary-title");
-let backgroundAbovePC = document.querySelector('.background-glow-after-scroll');
+let apearence = function (objectToApear, scrollScale, pointOfApearence) {
+    if (scrollScale > pointOfApearence) {
+        objectToApear.classList.add('animated-apearence');
+        setTimeout(() => {
+            objectToApear.style.setProperty('opacity', '1');
+        }, 600);
+    } else {
+        objectToApear.style.setProperty('opacity', '0');
+        objectToApear.classList.remove('animated-apearence');
+    }
+};
 
 window.addEventListener('scroll', () => {
     let scrollScale = document.documentElement.scrollTop;
+    let aboutSectionTitle = document.querySelector("#about-section .secondary-title");
+    let skillsTitle = document.querySelector("#skills-section .secondary-title");
+    let backgroundAbovePC = document.querySelector('.background-glow-after-scroll');
 
-    if (scrollScale > 60) {
-        aboutSectionTitle.classList.add('animated-apearence');
-        setTimeout(() => {
-            aboutSectionTitle.style.setProperty('opacity', '1');
-        }, 600);
-    } else {
-        aboutSectionTitle.style.setProperty('opacity', '0');
-        aboutSectionTitle.classList.remove('animated-apearence');
-    }
+    apearence(aboutSectionTitle, scrollScale, 60);
+    apearence(skillsTitle, scrollScale, 1650);
 
     if (scrollScale > 100) {
         opacityScale = (scrollScale - 140) / 600;
